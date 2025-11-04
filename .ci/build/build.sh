56f5d576da3b4d10bbda1297083d5e67f85c81fc
huydoan <doanquochuy021203@gmail.com>
2025-11-04 13:45:19 +0700

    [tag]staging

--- .ci/build/build.sh
+++ .ci/build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd .ci
-sudo docker build -t $image_name:$tag --build-arg -f $docker_path/build.Dockerfile ../
+sudo docker build -t $image_name:$tag -f $docker_path/build.Dockerfile ../
 sudo docker logout registry.gitlab.com
 echo "$CI_REGISTRY_PW" | sudo docker login registry.gitlab.com -u "$CI_REGISTRY_USER" --password-stdin
 sudo docker images
