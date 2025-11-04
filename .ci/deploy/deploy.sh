0668cb416137f53bb63f5da153ba034b3263d5cd
Đoàn Quốc Huy <doanquochuy021203@gmail.com>
2025-11-04 21:36:04 +0700

    [tag]staging

--- .ci/deploy/deploy.sh
+++ .ci/deploy/deploy.sh
@@ -4,6 +4,7 @@ sed -i 's|PROJECT-NAME|'$project_name'|' docker-compose.yaml
 sed -i 's|ENVIRONMENT-NAME|'$environment_name'|' docker-compose.yaml
 sed -i 's|IMAGE-NAME|'$image_name'|' docker-compose.yaml
 sed -i 's|IMAGE-TAG|'$tag'|' docker-compose.yaml
+sed -i 's|PORT-MAPPING|'$port_mapping'|' docker-compose.yaml
 #sed -i 's|MOUNT-DATA-FOLDER|'$mount_data_folder'|' docker-compose.yaml
 echo "$CI_REGISTRY_PW" | sudo docker login registry.gitlab.com -u "$CI_REGISTRY_USER" --password-stdin
 sudo docker-compose pull
