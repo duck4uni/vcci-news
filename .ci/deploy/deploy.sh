3816617341751e5d5352437ffd77af8eb66718cb
huydoan <doanquochuy021203@gmail.com>
2025-11-04 13:36:03 +0700

    [tag]staging

--- .ci/deploy/deploy.sh
+++ .ci/deploy/deploy.sh
@@ -4,8 +4,8 @@ jq -r 'keys[] as $k | "\($k)=\(.[$k])"' $environment_json_path > .env
 sed -i 's|PROJECT-NAME|'$project_name'|' docker-compose.yaml
 sed -i 's|ENVIRONMENT-NAME|'$environment_name'|' docker-compose.yaml
 sed -i 's|IMAGE-NAME|'$image_name'|' docker-compose.yaml
-sed -i 's|IMAGE-TAG|'staging-admin-$tag'|' docker-compose.yaml
-sed -i 's|MOUNT-DATA-FOLDER|'$mount_data_folder'|' docker-compose.yaml
+sed -i 's|IMAGE-TAG|'$tag'|' docker-compose.yaml
+#sed -i 's|MOUNT-DATA-FOLDER|'$mount_data_folder'|' docker-compose.yaml
 echo "$CI_REGISTRY_PW" | sudo docker login registry.gitlab.com -u "$CI_REGISTRY_USER" --password-stdin
 sudo docker-compose pull
 sudo docker-compose down
--- .ci/env-param.sh
+++ .ci/env-param.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-export project_name="develop-mepharco"
+export project_name="vcci-news"
 export environment_json_path=".env"
-export image_name="registry.gitlab.com/meu-solutions/ecommerce"
-export port_mapping="8110"
-export port_mapping_admin="8111"
-export environment_name="staging"
-export img_bak_tag="old"
+export image_name="registry.gitlab.com/meu-solutions/vcci-news"
+export port_mapping="3100"
