ee911471fabaf892a664496e5ac013460cecd2b6
huydoan <doanquochuy021203@gmail.com>
2025-11-04 15:15:08 +0700

    [tag]staging

--- .ci/env-param.sh
+++ .ci/env-param.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export project_name="vcci-news"
 export environment_json_path=".env"
-export image_name="registry.gitlab.com/meu-solutions/vcci-news"
+export image_name="registry.gitlab.com/meusolutions/vcci-news"
 export port_mapping="3100"
