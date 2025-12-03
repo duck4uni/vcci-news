b9d5e5c235095a3c97bbc4524ac4b785518dfcbe
baophm03 <baoisme2003@gmail.com>
2025-12-03 15:23:25 +0700

    a

--- src/links/index.ts
+++ src/links/index.ts
@@ -1,13 +1,10 @@
-const backendHost = '103.72.98.149:7041'
-
 const links = {
   analyticsGoogle: 'G-C9TEK9BS4C',
-  // apiEndpoint: "http://localhost:3000/api/v1.0",
-  apiEndpoint: `http://${backendHost}/api/v1.0`,
-  // imageEndpoint: 'https://utc2.erp.meu-solutions.com',
-  imageEndpoint: `http://${backendHost}`,
-  backendHost,
+  apiEndpoint: `https://hiea.meu-solutions.com/api/v1.0`,
+  imageEndpoint: `http://103.72.98.149:7041`,
   siteURL: 'https://hiea-news.meu-solutions.com/',
 }
 
 export default links
+
+
